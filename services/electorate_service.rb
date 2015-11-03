d29cad196a2b6d527cf047c9c2cd4518cc4a132c
austiine04 <austiine04@gmail.com>
2015-11-03 17:00:10 +1100

    now fetches mesh blocks

--- services/electorate_service.rb
+++ services/electorate_service.rb
@@ -3,22 +3,40 @@ require 'httparty'
 class ElectorateService
   include HTTParty
 
-  def initialize(electorate_name)
-    @electorate_name = electorate_name
+  def initialize(electorate_id)
+    @electorate_id = electorate_id
   end
 
   def request_payload
     {
-      fields: [],
-      query: {
-        term: {
-            slug: @electorate_name
+      "from": 0,
+      "size": 3000,
+      "query": {
+        "filtered": {
+        "query": {
+          "match": {
+             "type": "MeshBlock"
+          }
+        },
+        "filter": {
+            "geo_shape": {
+               "location": {
+                   "indexed_shape": {
+                       "id": @electorate_id,
+                       "index": "territories",
+                       "type": "territory",
+                       "path": "location"
+                   }
+               }
+             }
+          }
         }
-      }
     }
+  }
   end
 
-  def get_id
-    ElectorateService.post('https://site:a1534a534ef72b948437133ae441e134@kili-eu-west-1.searchly.com/territories/territory/_search', body: request_payload.to_json)
+  def get_mesh_blocks
+    response = ElectorateService.get('https://site:a1534a534ef72b948437133ae441e134@kili-eu-west-1.searchly.com/_search', body: request_payload.to_json)
+    response.body
   end
 end
