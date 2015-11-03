05204fc5b44262d5fd59ba1f0d5b285a454a753a
Arun Murali <amurali@thoughtworks.com>
2015-11-03 16:45:27 +1100

    Updates map, interactive mesh boxes

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -1,40 +1,74 @@
 var draw_map = function(x, y) {
   var map = L.map('map').setView([x, y], 4);
 
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  //L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
-  var style = function(feature) {
-    return {
-      weight: 2,
-      opacity: 1,
-      color: 'white',
-      dashArray: '3',
-      //fillColor: '#DDA0DD',
-      fillColor: '#E6FF00',
-      fillOpacity: 0.5,
-    }
-  }
 
-  $.get("https://gist.githubusercontent.com/tjmcewan/ce917fb3af63a4700426/raw/70828859b4493f241e32ae2beb9beaa3f691252a/response.json", function(body) {
-    var parsed_body = $.parseJSON(body);
-    for(var i = 0; i < parsed_body.hits.hits.length; i++) {
-      var polygon = L.geoJson(parsed_body.hits.hits[i]._source.location, { style: style }).addTo(map);
+
+  //$.get("https://gist.githubusercontent.com/tjmcewan/ce917fb3af63a4700426/raw/70828859b4493f241e32ae2beb9beaa3f691252a/response.json", function(body) {
+//      var polygon = L.geoJson(parsed_body.hits.hits[i]._source.location, { style: style }).addTo(map);
+  $.get("https://gist.githubusercontent.com/tjmcewan/ccefff4ee4baad9fc555/raw/1924f6bcc4b5e4a2c95790d1b7c207cf784cf1c5/inside_territories.json", function(body) {
+    var mesh_boxes;
+    var selected_boxes = new Object();
+
+    var style = function(feature) {
+      //console.log("Feature " + feature.properties.slug);
+      return {
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        //fillColor: '#DDA0DD',
+        //fillColor: '#E6FF00',
+        fillColor: '#F0054C',
+        fillOpacity: 0.5,
+      }
     }
 
-    L.marker([-28.5381470905, 150.286818304]).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
-//L.multipolygon(body["hits"]["hits"][0]
+    var hightlightMeshBox = function(e) {
+      var layer = e.target;
+
+      layer.setStyle({
+        weight: 3,
+        color: '#666',
+        dashArray: '',
+        fillColor: '#E6FF00',
+      });
+
+      if (!L.Browser.ie && !L.Browser.opera) {
+        layer.bringToFront();
+      }
+    }
+
+    var resetMeshBox = function(e) {
+      mesh_boxes.resetStyle(e.target);
+    }
+
+    /*
+    var selectMeshBox = function(e) {
+      puts "
+    }
+    */
+
+    var onEachFeature = function(feature, layer) {
+      layer.on({
+        mouseover: hightlightMeshBox,
+        mouseout: resetMeshBox,
+     //   click: selectMeshBox,
+      });
+    }
+
+    mesh_boxes = L.geoJson(
+                    {"type": 'FeatureCollection', "features": $.parseJSON(body)},
+                    { style: style, onEachFeature: onEachFeature }
+                  ).addTo(map);
+
+    map.fitBounds(mesh_boxes.getBounds());
+
   })
-
-  /*
-  L.marker([-29.8650, 131.2094]).addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
-      */
-
 }
 
 $('#map').height($(window).height());
