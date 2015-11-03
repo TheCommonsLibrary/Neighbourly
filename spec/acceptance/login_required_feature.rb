7b0b404446926e1ca98ad9a2c7d4f402e8f317e6
Cherry <cherryzh@thoughtworks.com>
2015-11-03 16:20:19 +1100

    acceptance_test_login_without_crendentials

--- spec/acceptance/login_required_feature.rb
+++ spec/acceptance/login_required_feature.rb
@@ -1,13 +1,9 @@
 
 #happy path
-describe 'Login Feature' do  
-  context 'the login URL' do
+describe 'Login Feature' do 
 
-    # before (:each) do
-    #   visit 'http://localhost:4567'
-    # end
-
-#sad path with invalid nation name
+  describe 'without credentials' do
+  
     it "should fail the login with valid invalid credentials" do
         visit 'http://localhost:4567'
         fill_in('nation-input', :with => 'Victoria')
@@ -15,32 +11,22 @@ describe 'Login Feature' do
         expect(page).to have_content "We couldn't find the page you were looking for."
     end
 
-#sad path with valid nation name but invalid credentials
-      it "should fail the login with invalid invalid credentials" do
-          visit 'http://localhost:4567'
-          fill_in('nation-input', :with => 'getupstaging')
-          find_button('login-button').click
-          expect(current_url).to match(/https:\/\/getupstaging.nationbuilder.com/)
-
-          find('#user_session_email').set('cherryzh@gmail.com')
-          find('#user_session_password').set('123')
-          first('input[name="commit"]').click
-          expect(page).to have_content "1 ERROR OCCURRED WHILE PROCESSING THIS FORM."
-    end
-
-#happy path
-    it "should login successfully with valid credentials" do
+    it "should fail the login with invalid invalid credentials" do
         visit 'http://localhost:4567'
         fill_in('nation-input', :with => 'getupstaging')
         find_button('login-button').click
         expect(current_url).to match(/https:\/\/getupstaging.nationbuilder.com/)
 
-        find('#user_session_email').set('ili@thoughtworks.com')
-        find('#user_session_password').set('11qazwsxedc')
+        find('#user_session_email').set('cherryzh@gmail.com')
+        find('#user_session_password').set('123')
         first('input[name="commit"]').click
-        expect(current_url).to eq('http://localhost:4567/electorates')
+        expect(page).to have_content "1 ERROR OCCURRED WHILE PROCESSING THIS FORM."
     end
-
+    
+    it "should redirect to login page" do
+        visit 'http://localhost:4567/map'
+        expect(page).to have_content "You need to login before you can view that page."
+    end
   end
 end
-
+  
