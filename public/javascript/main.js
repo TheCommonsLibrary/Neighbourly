1ae7de0109338ad5757f56a32285e04f9ebd91ef
richard <richard@getup.org.au>
2015-11-03 17:25:16 +1100

    select the electorate depending on the map that is shown

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,5 +1,18 @@
 $(function() {
+  function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+
+  var electorate = getParameterByName('electorate');
+  $('.electorate-picker select option[value="' + electorate + '"]').prop('selected', true);
+
   $('.electorate-picker select').change(function() {
+    if ($('.electorate-picker select').val() === "") {
+      return;
+    }
     window.location.href = '/map?electorate=' + $('.electorate-picker select').val();
   });
 });
