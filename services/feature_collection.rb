127766489079227513d64f53862d749972bc08d7
austiine04 <austiine04@gmail.com>
2015-11-05 01:17:54 +1100

    better name for the method

--- services/feature_collection.rb
+++ services/feature_collection.rb
@@ -8,21 +8,21 @@ class FeatureCollection
 
   def to_a
     @mesh_blocks.map do |mesh_block|
-      claimed_by = @mesh_block_claimers[mesh_block['_source']['slug']]
+      claimed_by = @mesh_block_claimers[mesh_block['_source']['slug']] # what exactly is this? we need a better name
       {
         type: 'Feature',
         geometry: mesh_block['_source']['location'],
         properties: {
           slug: mesh_block['_source']['slug'],
           type: mesh_block['_source']['type'],
-          claimedBy: get_claimed_by_flag_from(claimed_by)
+          claimedBy: define_claimed_by_status_from(claimed_by)
         }
       }
     end
   end
 
   private
-  def get_claimed_by_flag_from(claimed_by)
+  def define_claimed_by_status_from(claimed_by)
     return 'unclaimed' if claimed_by.nil?
     claimed_by == @nation_slug ? 'selected' : 'claimed'
   end
