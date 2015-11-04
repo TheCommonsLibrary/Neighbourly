4c10ee8aa4f3a92971aa4e945c48930f40941ff1
austiine04 <austiine04@gmail.com>
2015-11-05 01:45:51 +1100

    move mesh block query to elastic search namespace

---
+++ services/elastic_search/mesh_block_query.rb
@@ -0,0 +1,41 @@
+require_relative './connection'
+
+module ElasticSearch
+  module Query
+    class MeshBlocksQuery
+      def initialize(electorate_id)
+        @elastic_search = ElasticSearch::Connection.new
+        @electorate_id = electorate_id
+        @query = {
+          "from": 0,
+          "size": 3000,
+          "query": {
+            "filtered": {
+              "query": {
+                "match": {
+                  "type": "MeshBlock"
+                }
+              },
+              "filter": {
+                "geo_shape": {
+                  "location": {
+                    "indexed_shape": {
+                      "id": @electorate_id,
+                      "index": "territories",
+                      "type": "territory",
+                      "path": "location"
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      end
+
+      def execute
+        @elastic_search.execute @query
+      end
+    end
+  end
+end
--- services/mesh_block_service.rb
+++ services/mesh_block_service.rb
@@ -1,10 +1,10 @@
-require_relative './mesh_block_query'
+require_relative './elastic_search/mesh_block_query'
 require_relative './../models/mesh_blocks_claim'
 require_relative './../models/feature_collection'
 
 class MeshBlockService
   def initialize(electorate_id, db, nation_slug)
-    @query_results = MeshBlocksQuery.new(electorate_id).execute
+    @query_results = ElasticSearch::Query::MeshBlocksQuery.new(electorate_id).execute
     claim = MeshBlockClaim.new(db, @query_results['hits']['hits'])
     @feature_collection = FeatureCollection.new @query_results, nation_slug, claim.get_claimers
   end
