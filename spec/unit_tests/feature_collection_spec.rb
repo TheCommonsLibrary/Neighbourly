1a93ad54242bfde6c3caa7d1dfe81708d5f49448
Isabella Li <yiman@Isabellas-MacBook-Pro.local>
2015-11-05 16:49:06 +1100

    add unit test for feature collection

---
+++ spec/unit_tests/feature_collection_spec.rb
@@ -0,0 +1,120 @@
+require_relative '../../models/feature_collection'
+
+describe 'FeatureCollection' do
+
+	describe '#to_a' do
+
+		let(:db) { RSpec.configuration.db }
+		
+		before :each do
+			db[:claims].insert(:mesh_block_slug => 'slug1', :mesh_block_claimer => 'user 1', :claim_date => Time.now)
+			db[:claims].insert(:mesh_block_slug => 'slug2', :mesh_block_claimer => 'user 2', :claim_date => Time.now)
+		end
+
+		let(:query_result) {
+			{
+			 "hits" => {
+			   "total" => 460,
+			   "max_score" => 1.0030303,
+			   "hits" => [
+			     {
+			       "_index" => "territories",
+			       "_type" => "territory",
+			       "_id" => "299974",
+			       "_score" => 1.0030303,
+			       "_source" => {
+			         "slug" => "slug1",
+			         "type" => "MeshBlock",
+			         "location" => {
+			           "type" => "MultiPolygon",
+			           "coordinates" => [
+			             [
+			               [
+			                 [
+			                   150.286818304,
+			                   -28.5381470905
+			                 ]
+			               ]
+			             ]]
+			          }
+			        }
+			      },
+			      {
+			       "_index" => "territories",
+			       "_type" => "territory",
+			       "_id" => "299974",
+			       "_score" => 1.0030303,
+			       "_source" => {
+			         "slug" => "slug2",
+			         "type" => "MeshBlock",
+			         "location" => {
+			           "type" => "MultiPolygon",
+			           "coordinates" => [
+			             [
+			               [
+			                 [
+			                   150.286818304,
+			                   -28.5381470905
+			                 ]
+			               ]
+			             ]]
+			          }
+			        }
+			      },
+			      {
+			       "_index" => "territories",
+			       "_type" => "territory",
+			       "_id" => "299974",
+			       "_score" => 1.0030303,
+			       "_source" => {
+			         "slug" => "slug3",
+			         "type" => "MeshBlock",
+			         "location" => {
+			           "type" => "MultiPolygon",
+			           "coordinates" => [
+			             [
+			               [
+			                 [
+			                   150.286818304,
+			                   -28.5381470905
+			                 ]
+			               ]
+			             ]]
+			          }
+			        }
+			      },
+	        ]
+        }
+      }
+		}
+
+		let(:mesh_block_claimers) {
+			{
+				"slug1"=>"user 1", 
+				"slug2"=>"user 2",
+			}
+		}
+
+		it 'should convert raw query data into correct mash block format' do
+			feature_collection = FeatureCollection.new query_result, 'user 1', mesh_block_claimers
+
+			mesh_blocks = feature_collection.to_a
+
+			expect(mesh_blocks[0]).to eq({:type=>"Feature",
+																		:geometry=>{
+																			"type"=>"MultiPolygon",
+																			"coordinates"=>[[[[150.286818304, -28.5381470905]]]]},
+													          :properties=>{
+													          	:slug=>"slug1",
+													          	:type=>"MeshBlock",
+													          	:claimedBy=>"user 1",
+													          	:state=>"selected"}
+													          })
+
+			expect(mesh_blocks[1][:properties][:state]).to eq('claimed')
+			expect(mesh_blocks[2][:properties][:state]).to eq('unclaimed')
+			db[:claims].delete
+		end
+
+	end
+end
