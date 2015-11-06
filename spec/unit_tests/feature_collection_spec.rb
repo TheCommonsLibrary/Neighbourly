0519d150ac3dbd3546f0550bf0b9847cb57f2367
Tim McEwan <tim@mcewan.it>
2015-11-06 15:51:46 +1100

    fix specs; normalise files

--- spec/unit_tests/feature_collection_spec.rb
+++ spec/unit_tests/feature_collection_spec.rb
@@ -1,120 +1,126 @@
-require_relative '../../models/feature_collection'
+require_relative "../../models/feature_collection"
 
-describe 'FeatureCollection' do
+describe "FeatureCollection" do
 
-	describe '#to_a' do
+  describe "#to_a" do
 
-		let(:db) { RSpec.configuration.db }
-		
-		before :each do
-			db[:claims].insert(:mesh_block_slug => 'slug1', :mesh_block_claimer => 'user 1', :claim_date => Time.now)
-			db[:claims].insert(:mesh_block_slug => 'slug2', :mesh_block_claimer => 'user 2', :claim_date => Time.now)
-		end
+    let(:db) { RSpec.configuration.db }
 
-		let(:query_result) {
-			{
-			 "hits" => {
-			   "total" => 460,
-			   "max_score" => 1.0030303,
-			   "hits" => [
-			     {
-			       "_index" => "territories",
-			       "_type" => "territory",
-			       "_id" => "299974",
-			       "_score" => 1.0030303,
-			       "_source" => {
-			         "slug" => "slug1",
-			         "type" => "MeshBlock",
-			         "location" => {
-			           "type" => "MultiPolygon",
-			           "coordinates" => [
-			             [
-			               [
-			                 [
-			                   150.286818304,
-			                   -28.5381470905
-			                 ]
-			               ]
-			             ]]
-			          }
-			        }
-			      },
-			      {
-			       "_index" => "territories",
-			       "_type" => "territory",
-			       "_id" => "299974",
-			       "_score" => 1.0030303,
-			       "_source" => {
-			         "slug" => "slug2",
-			         "type" => "MeshBlock",
-			         "location" => {
-			           "type" => "MultiPolygon",
-			           "coordinates" => [
-			             [
-			               [
-			                 [
-			                   150.286818304,
-			                   -28.5381470905
-			                 ]
-			               ]
-			             ]]
-			          }
-			        }
-			      },
-			      {
-			       "_index" => "territories",
-			       "_type" => "territory",
-			       "_id" => "299974",
-			       "_score" => 1.0030303,
-			       "_source" => {
-			         "slug" => "slug3",
-			         "type" => "MeshBlock",
-			         "location" => {
-			           "type" => "MultiPolygon",
-			           "coordinates" => [
-			             [
-			               [
-			                 [
-			                   150.286818304,
-			                   -28.5381470905
-			                 ]
-			               ]
-			             ]]
-			          }
-			        }
-			      },
-	        ]
+    before :each do
+      db[:users].insert(email: "user1@example.com")
+      db[:users].insert(email: "user2@example.com")
+      db[:claims].insert(mesh_block_slug: "slug1", mesh_block_claimer: "user1@example.com", claim_date: Time.now)
+      db[:claims].insert(mesh_block_slug: "slug2", mesh_block_claimer: "user2@example.com", claim_date: Time.now)
+    end
+
+    after :each do
+      db[:claims].delete
+      db[:users].delete
+    end
+
+    let(:query_result) {
+      {
+       "hits" => {
+         "total" => 460,
+         "max_score" => 1.0030303,
+         "hits" => [
+           {
+             "_index" => "territories",
+             "_type" => "territory",
+             "_id" => "299974",
+             "_score" => 1.0030303,
+             "_source" => {
+               "slug" => "slug1",
+               "type" => "MeshBlock",
+               "location" => {
+                 "type" => "MultiPolygon",
+                 "coordinates" => [
+                   [
+                     [
+                       [
+                         150.286818304,
+                         -28.5381470905
+                       ]
+                     ]
+                   ]]
+                }
+              }
+            },
+            {
+             "_index" => "territories",
+             "_type" => "territory",
+             "_id" => "299974",
+             "_score" => 1.0030303,
+             "_source" => {
+               "slug" => "slug2",
+               "type" => "MeshBlock",
+               "location" => {
+                 "type" => "MultiPolygon",
+                 "coordinates" => [
+                   [
+                     [
+                       [
+                         150.286818304,
+                         -28.5381470905
+                       ]
+                     ]
+                   ]]
+                }
+              }
+            },
+            {
+             "_index" => "territories",
+             "_type" => "territory",
+             "_id" => "299974",
+             "_score" => 1.0030303,
+             "_source" => {
+               "slug" => "slug3",
+               "type" => "MeshBlock",
+               "location" => {
+                 "type" => "MultiPolygon",
+                 "coordinates" => [
+                   [
+                     [
+                       [
+                         150.286818304,
+                         -28.5381470905
+                       ]
+                     ]
+                   ]]
+                }
+              }
+            },
+          ]
         }
       }
-		}
+    }
 
-		let(:mesh_block_claimers) {
-			{
-				"slug1"=>"user 1", 
-				"slug2"=>"user 2",
-			}
-		}
+    let(:mesh_block_claimers) {
+      {
+        "slug1"=>{email: "user1@example.com"},
+        "slug2"=>{email: "user2@example.com"},
+      }
+    }
 
-		it 'should convert raw query data into correct mash block format' do
-			feature_collection = FeatureCollection.new query_result, 'user 1', mesh_block_claimers
+    it "should convert raw query data into correct mash block format" do
+      feature_collection = FeatureCollection.new query_result, "user1@example.com", mesh_block_claimers
 
-			mesh_blocks = feature_collection.to_a
+      mesh_blocks = feature_collection.to_a
 
-			expect(mesh_blocks[0]).to eq({:type=>"Feature",
-																		:geometry=>{
-																			"type"=>"MultiPolygon",
-																			"coordinates"=>[[[[150.286818304, -28.5381470905]]]]},
-													          :properties=>{
-													          	:slug=>"slug1",
-													          	:type=>"MeshBlock",
-													          	:claimedBy=>"user 1",
-													          	:state=>"selected"}
-													          })
+      expect(mesh_blocks[0]).to eq({type: "Feature",
+                                    geometry: {
+                                      "type"=>"MultiPolygon",
+                                      "coordinates"=>[[[[150.286818304, -28.5381470905]]]]},
+                                    properties: {
+                                      slug: "slug1",
+                                      type: "MeshBlock",
+                                      claimedBy: {email: "user1@example.com"},
+                                      state: "selected"}
+                                    })
 
-			expect(mesh_blocks[1][:properties][:state]).to eq('claimed')
-			expect(mesh_blocks[2][:properties][:state]).to eq('unclaimed')
-			db[:claims].delete
-		end
+      expect(mesh_blocks[1][:properties][:state]).to eq("claimed")
+      expect(mesh_blocks[2][:properties][:state]).to eq("unclaimed")
+    end
 
-	end
+  end
 end
--- spec/unit_tests/services/claim_service_spec.rb
+++ spec/unit_tests/services/claim_service_spec.rb
@@ -1,94 +1,79 @@
-require_relative '../../../services/claim_service'
+require_relative "../../../services/claim_service"
 
-describe 'ClaimService' do
+describe "ClaimService" do
 
-	let(:db) { RSpec.configuration.db }
-		
-	before :each do
-		db[:claims].insert(:mesh_block_slug => 'slug1', :mesh_block_claimer => 'user1', :claim_date => Time.now)
-		db[:claims].insert(:mesh_block_slug => 'slug2', :mesh_block_claimer => 'user2', :claim_date => Time.now)
-		db[:claims].insert(:mesh_block_slug => 'slug3', :mesh_block_claimer => 'user1', :claim_date => Time.now)
-		@claim_service = ClaimService.new(db)
-	end
+  let(:db) { RSpec.configuration.db }
 
-  after :each do
-			db[:claims].delete
+  before :each do
+    db[:users].insert(email: "user1@example.com")
+    db[:users].insert(email: "user2@example.com")
+    db[:claims].insert(mesh_block_slug: "slug1", mesh_block_claimer: "user1@example.com", claim_date: Time.now)
+    db[:claims].insert(mesh_block_slug: "slug2", mesh_block_claimer: "user2@example.com", claim_date: Time.now)
+    db[:claims].insert(mesh_block_slug: "slug3", mesh_block_claimer: "user1@example.com", claim_date: Time.now)
+    @claim_service = ClaimService.new(db)
   end
 
-	let(:mesh_blocks) {
-		[
-	     {
-	       "_source" => {
-	         "slug" => "slug1",
-	         "type" => "MeshBlock",
-	       }
-	      },
-	      {
-	       "_source" => {
-	         "slug" => "slug2",
-	         "type" => "MeshBlock",
-	       }
-	      },
-	      {
-	       "_source" => {
-	         "slug" => "slug3",
-	         "type" => "MeshBlock",
-	       }
-	      }
-    	]
-	}
+  after :each do
+    db[:claims].delete
+    db[:users].delete
+  end
 
-	describe '#get_claimers_for' do
-		it 'should get claimers for mesh_blocks' do
-			expect(@claim_service.get_claimers_for(mesh_blocks)).to eq({"slug1"=>"user1", "slug2"=>"user2", "slug3"=>"user1"})
-		end
-	end
+  let(:mesh_blocks) {
+    [
+       {
+         "_source" => {
+           "slug" => "slug1",
+           "type" => "MeshBlock",
+         }
+        },
+        {
+         "_source" => {
+           "slug" => "slug2",
+           "type" => "MeshBlock",
+         }
+        },
+        {
+         "_source" => {
+           "slug" => "slug3",
+           "type" => "MeshBlock",
+         }
+        }
+      ]
+  }
 
-	describe '#get_mesh_blocks_for' do
-		it 'should get mesh_blocks for a claimer' do
-			expect(@claim_service.get_mesh_blocks_for("user1")).to eq(["slug1", "slug3"])
-		end
-	end
+  describe "#get_claimers_for" do
+    it "should get claimers for mesh_blocks" do
+      expect(@claim_service.get_claimers_for(mesh_blocks)).to eq({"slug1"=>{email: "user1@example.com", name: nil, organisation: nil, phone: nil, created_at: nil}, "slug2"=>{email: "user2@example.com", name: nil, organisation: nil, phone: nil, created_at: nil}, "slug3"=>{email: "user1@example.com", name: nil, organisation: nil, phone: nil, created_at: nil}})
+    end
+  end
 
-  describe '#get_when_claimed_by_others' do
-    let(:user) { 'user1' }
-    context 'when not claimed by others' do
-      let(:block_slugs) { [ 'slug4', 'slug4' ] }
+  describe "#get_mesh_blocks_for" do
+    it "should get mesh_blocks for a claimer" do
+      expect(@claim_service.get_mesh_blocks_for("user1@example.com")).to eq(["slug1", "slug3"])
+    end
+  end
+
+  describe "#get_when_claimed_by_others" do
+    let(:user) { "user1@example.com" }
+    context "when not claimed by others" do
+      let(:block_slugs) { [ "slug4", "slug4" ] }
       it { expect(@claim_service.get_when_claimed_by_others(block_slugs, user)).to be_empty }
     end
 
-    context 'when not claimed by others' do
-      let(:block_slugs) { [ 'slug1', 'slug2', 'slug3' ] }
-      it { expect(@claim_service.get_when_claimed_by_others(block_slugs, user)).to contain_exactly(contain_exactly('slug2', 'user2')) }
+    context "when not claimed by others" do
+      let(:block_slugs) { [ "slug1", "slug2", "slug3" ] }
+      it { expect(@claim_service.get_when_claimed_by_others(block_slugs, user)).to contain_exactly(contain_exactly("slug2", "user2@example.com")) }
     end
   end
 
-	describe '#claim' do
-		it 'should save mesh block slug and claimer into database' do
-			mesh_blocks = ["mesh_block_1", "mesh_block_2"]
-			claimer = 'claimer'
-			@claim_service.claim(mesh_blocks, claimer)
+  describe "#claim" do
+    it "should save mesh block slug and claimer into database" do
+      mesh_blocks = ["mesh_block_1", "mesh_block_2"]
+      claimer = "claimer"
+      @claim_service.claim(mesh_blocks, claimer)
 
-			expect(@claim_service.get_mesh_blocks_for(claimer)).to eq(["mesh_block_1", "mesh_block_2"])
-			db[:claims].delete
-		end
-	end
+      expect(@claim_service.get_mesh_blocks_for(claimer)).to eq(["mesh_block_1", "mesh_block_2"])
+      db[:claims].delete
+    end
+  end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
